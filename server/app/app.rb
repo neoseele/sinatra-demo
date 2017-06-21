f3a5e71f7aae3c889f0b0d92412fee3c6da0a2a6
Neil Miao <nerv.2015@gmail.com>
2017-06-21 14:40:01 +1000

    add /now endpoint

--- server/app/app.rb
+++ server/app/app.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'date'
 
 get '/' do
-  "Pod: #{`hostname`.strip}"
+  "pod => #{`hostname`.strip}"
+end
+
+get '/now' do
+  json DateTime.now.to_s
 end
