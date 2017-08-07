d968d5d2fc9f8ce941e1f733f0861743b94861f0
Neil Miao <nerv.2015@gmail.com>
2017-08-07 18:16:15 +1000

    test

--- server/app/app.rb
+++ server/app/app.rb
@@ -49,7 +49,8 @@ class SimpleApp < Sinatra::Base
   end
 
   get '/stress' do
-    puts `dd if=/dev/zero of=/dev/null bs=10240 count=1000 2>&1`
+    out=`dd if=/dev/zero of=/dev/null bs=10240 count=1000 2>&1`
+    "#{out}!"
   end
 
   ### protected endpoints
