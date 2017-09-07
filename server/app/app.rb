26fdf2382434521c3f8d89dec9e91520264b6825
Neil Miao <nmiu@google.com>
2017-09-07 22:13:25 +1000

    test

--- server/app/app.rb
+++ server/app/app.rb
@@ -57,7 +57,7 @@ class SimpleApp < Sinatra::Base
   ### protected endpoints
 
   get '/hostinfo' do
-    protected!
+    # protected!
     json hostinfo
   end
 
--- server/app/helpers/hostinfo.rb
+++ server/app/helpers/hostinfo.rb
@@ -3,21 +3,20 @@ require 'open-uri'
 
 module Sinatra
   module HostinfoHelper
-    METADATA = 'http://metadata.google.internal/computeMetadata/v1beta1/instance'
     FORWARDED_IPS = 'network-interfaces/0/forwarded-ips/'
 
     def hostinfo
       info = {}
       info[:hostname] = read_metadata('hostname')[0]
-      info[:ip] = read_metadata('0/ip')[0]
-      info[:forwarded_ips] = read_metadata(FORWARDED_IPS).map do |i|
-        read_metadata("#{FORWARDED_IPS}/#{i}")[0]
-      end
-      info
+      # info[:ip] = read_metadata('0/ip')[0]
+      # info[:forwarded_ips] = read_metadata(FORWARDED_IPS).map do |i|
+      #   read_metadata("#{FORWARDED_IPS}/#{i}")[0]
+      # end
+      # info
     end
 
     def read_metadata(path)
-      open("#{METADATA}/#{path}",
+      open("http://metadata.google.internal/computeMetadata/v1beta1/instance/#{path}",
         'Metadata-Flavor' => 'Google').readlines.map do |l|
         l.strip
       end
